47737354c92ca14f85e90bb9b1140614457c4c42
Nishant Yadav <nishantyaduvanshi056@gmail.com>
2026-02-16 18:23:12 +0530

    fixes

--- setup-project.ps1
+++ setup-project.ps1
@@ -5,7 +5,7 @@ New-Item -ItemType Directory -Name CODEREVGENAI
 New-Item -ItemType Directory -Name CODEREVGENAI/backend
 New-Item -ItemType File -Path "CODEREVGENAI/backend/main.py"
 New-Item -ItemType File -Path "CODEREVGENAI/backend/requirements.txt"
-"fastapi`ngroq`npython-dotenv" | Out-File -FilePath "CODEREVGENAI/backend/requirements.txt"
+"fastapi`nuvicorn`npython-dotenv`nmysql-connector-python`ngroq`nhttpx`nslowapi`npython-multipart`npython-docx`nfpdf`nPillow`npytesseract`ngoogle-generativeai`npasslib[bcrypt]`npython-jose[cryptography]" | Out-File -FilePath "CODEREVGENAI/backend/requirements.txt"
 New-Item -ItemType File -Path "CODEREVGENAI/backend/.env"
 "GROQ_API_KEY=" | Out-File -FilePath "CODEREVGENAI/backend/.env"
 New-Item -ItemType File -Path "CODEREVGENAI/backend/__init__.py"
